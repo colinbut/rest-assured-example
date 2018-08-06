a0b7aee3317db039d63df03db7ee92810e0ab668
Colin But <colinbut@users.noreply.github.com>
2018-08-06 10:00:40 +0100

    replace Spring framework with a Spring Boot setup

---
+++ src/main/java/com/mycompany/restassured/example/RestAssuredExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.restassured.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssuredExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAssuredExampleApplication.class, args);
+    }
+}
