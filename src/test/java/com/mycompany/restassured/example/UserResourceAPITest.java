2d672cb8dfc34827eaaa5c64778e40cd50ac7fc2
Colin But <colinbut@users.noreply.github.com>
2018-08-06 10:18:21 +0100

    filled in missing API test

--- src/test/java/com/mycompany/restassured/example/UserResourceAPITest.java
+++ src/test/java/com/mycompany/restassured/example/UserResourceAPITest.java
@@ -2,11 +2,27 @@ package com.mycompany.restassured.example;
 
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class UserResourceAPITest {
 
     @Test
     public void testGetUserDetails() {
-        System.out.println("To fill in this test");
+        when().
+            get("/user/{userId}", 1).
+        then().
+            statusCode(200).
+            body("userId", equalTo(1),
+                 "firstName", equalTo("Joe"),
+                 "secondName", equalTo("Bloggs"),
+                 "email", equalTo("me@email.com"),
+                 "address.firstLineAddress", equalTo("My First Line Address"),
+                 "address.secondLineAddress", equalTo("My Second Line Address"),
+                 "address.postCode", equalTo("MU3 28P"),
+                 "address.city", equalTo("Paris"),
+                 "address.country", equalTo("UK"));
     }
 
 }
