1c095cdb7bdb713063f3042388d64587d6d3037e
Colin But <colinbut@users.noreply.github.com>
2018-08-06 09:54:51 +0100

    fixing compiling error with missing import

--- src/main/java/com/mycompany/restassured/example/UserResource.java
+++ src/main/java/com/mycompany/restassured/example/UserResource.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
